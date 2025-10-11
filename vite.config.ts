7d4759e37f11b60352844302d8e70a1051980995
Kolja Sam <github@koljapluemer.com>
2025-10-11 08:54:05 +0200

    add PWA stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vitest/config';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
-import tailwindcss from '@tailwindcss/vite';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
+import { VitePWA } from "vite-plugin-pwa";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), VitePWA()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   test: {
-    environment: 'jsdom',
-    exclude: ['**/node_modules/**', '**/dist/**', 'legacy/**']
-  }
+    environment: "jsdom",
+    exclude: ["**/node_modules/**", "**/dist/**", "legacy/**"],
+  },
 });
