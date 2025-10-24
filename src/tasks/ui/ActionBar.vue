e384bf3f0b502e0548b917d4f52da755b942d6c5
Kolja Sam <github@koljapluemer.com>
2025-10-24 10:02:35 +0200

    improve looks of ActionBar

--- src/tasks/ui/ActionBar.vue
+++ src/tasks/ui/ActionBar.vue
@@ -1,44 +1,39 @@
 <template>
-  <div
-    class="bg-primary border-t-10 border-t-secondary p-4 pt-20 text-white flex flex-row gap-4 justify-between items-end relative min-h-32">
-    <!-- Left Element -->
-    <div class="flex flex-row gap-2 items-center">
-      <!-- Default skip button -->
-      <button
-        v-if="!hideSkipButton"
-        @click="emit('action', 'skip')"
-        class="btn btn-sm btn-ghost text-white"
-      >
-        <component :is="getIcon('skip')" :size="20" />
-      </button>
-
-      <!-- Default disable button -->
-      <button
-        v-if="!hideDisableButton"
-        @click="emit('action', 'disable')"
-        class="btn btn-sm btn-ghost text-white"
-      >
-        <component :is="getIcon('disable')" :size="20" />
-      </button>
-
-      <!-- Default jump-to button -->
-      <button
-        v-if="!hideJumpToButton"
-        @click="emit('action', 'jump-to')"
-        class="btn btn-sm btn-ghost text-white"
-      >
-        <component :is="getIcon('jump-to')" :size="20" />
-      </button>
-
-      <!-- Additional left controls -->
-      <template v-for="control in secondaryLeftControls" :key="control.id">
+  <div class="relative">
+    <!-- Central controls - positioned at top, naturally sized -->
+    <div class="flex flex-col md:flex-row justify-center gap-2 px-4 relative z-10">
+      <template v-for="control in centralControls" :key="control.id">
         <button
           v-if="control.type === 'button'"
           @click="emit('action', control.id)"
           :class="getButtonClass(control.position, control.destructive)"
+          :disabled="control.disabled"
         >
           {{ control.label }}
         </button>
+        <button
+          v-else-if="control.type === 'image-button'"
+          @click="emit('action', control.id)"
+          :class="getImageButtonClass(control.position)"
+        >
+          <img :src="control.imageUrl" :alt="control.alt" class="w-full h-full object-cover" />
+        </button>
+        <input
+          v-else-if="control.type === 'text-input'"
+          type="text"
+          :value="control.value"
+          @input="emit('action', control.id, ($event.target as HTMLInputElement).value)"
+          :placeholder="control.placeholder"
+          class="input input-bordered input-lg"
+        />
+        <textarea
+          v-else-if="control.type === 'textarea'"
+          :value="control.value"
+          @input="emit('action', control.id, ($event.target as HTMLTextAreaElement).value)"
+          :placeholder="control.placeholder"
+          :disabled="control.disabled"
+          class="textarea w-96 h-40 text-xl border-3 border-secondary bg-primary text-white placeholder-white/60 resize-none"
+        />
         <button
           v-else-if="control.type === 'icon-button'"
           @click="emit('action', control.id)"
@@ -50,73 +45,79 @@
       </template>
     </div>
 
-    <!-- Central Element with Header and Footer -->
-    <div class="flex flex-col items-center gap-2 absolute left-1/2 -translate-x-1/2 bottom-0 pb-4">
-      <!-- Central controls -->
-      <div class="flex flex-row flex-wrap justify-center gap-2 items-center">
-        <template v-for="control in centralControls" :key="control.id">
+    <!-- Colored background section - negative margin to overlap central buttons -->
+    <div class="-mt-[30px] bg-primary border-t-10 border-t-secondary p-4 pt-[40px] text-white">
+      <div class="flex justify-between items-end gap-4">
+        <!-- Left controls -->
+        <div class="flex flex-row gap-2 items-center">
+          <!-- Default skip button -->
           <button
-            v-if="control.type === 'button'"
-            @click="emit('action', control.id)"
-            :class="getButtonClass(control.position, control.destructive)"
-            :disabled="control.disabled"
+            v-if="!hideSkipButton"
+            @click="emit('action', 'skip')"
+            class="btn btn-sm btn-ghost text-white"
           >
-            {{ control.label }}
+            <component :is="getIcon('skip')" :size="20" />
           </button>
+
+          <!-- Default disable button -->
           <button
-            v-else-if="control.type === 'image-button'"
-            @click="emit('action', control.id)"
-            :class="getImageButtonClass(control.position)"
+            v-if="!hideDisableButton"
+            @click="emit('action', 'disable')"
+            class="btn btn-sm btn-ghost text-white"
           >
-            <img :src="control.imageUrl" :alt="control.alt" class="w-full h-full object-cover" />
+            <component :is="getIcon('disable')" :size="20" />
           </button>
-          <input
-            v-else-if="control.type === 'text-input'"
-            type="text"
-            :value="control.value"
-            @input="emit('action', control.id, ($event.target as HTMLInputElement).value)"
-            :placeholder="control.placeholder"
-            class="input input-bordered input-lg"
-          />
-          <textarea
-            v-else-if="control.type === 'textarea'"
-            :value="control.value"
-            @input="emit('action', control.id, ($event.target as HTMLTextAreaElement).value)"
-            :placeholder="control.placeholder"
-            :disabled="control.disabled"
-            class="textarea w-96 h-40 text-xl border-3 border-secondary bg-primary text-white placeholder-white/60 resize-none"
-          />
+
+          <!-- Default jump-to button -->
           <button
-            v-else-if="control.type === 'icon-button'"
-            @click="emit('action', control.id)"
-            :class="getButtonClass(control.position)"
+            v-if="!hideJumpToButton"
+            @click="emit('action', 'jump-to')"
+            class="btn btn-sm btn-ghost text-white"
           >
-            <component :is="getIcon(control.icon)" />
-            <span v-if="control.label">{{ control.label }}</span>
+            <component :is="getIcon('jump-to')" :size="20" />
           </button>
-        </template>
+
+          <!-- Additional left controls -->
+          <template v-for="control in secondaryLeftControls" :key="control.id">
+            <button
+              v-if="control.type === 'button'"
+              @click="emit('action', control.id)"
+              :class="getButtonClass(control.position, control.destructive)"
+            >
+              {{ control.label }}
+            </button>
+            <button
+              v-else-if="control.type === 'icon-button'"
+              @click="emit('action', control.id)"
+              :class="getButtonClass(control.position)"
+            >
+              <component :is="getIcon(control.icon)" />
+              <span v-if="control.label">{{ control.label }}</span>
+            </button>
+          </template>
+        </div>
+
+        <!-- Right controls -->
+        <div class="flex flex-row gap-2 items-center">
+          <template v-for="control in secondaryRightControls" :key="control.id">
+            <button
+              v-if="control.type === 'button'"
+              @click="emit('action', control.id)"
+              :class="getButtonClass(control.position, control.destructive)"
+            >
+              {{ control.label }}
+            </button>
+            <button
+              v-else-if="control.type === 'icon-button'"
+              @click="emit('action', control.id)"
+              :class="getButtonClass(control.position)"
+            >
+              <component :is="getIcon(control.icon)" />
+              <span v-if="control.label">{{ control.label }}</span>
+            </button>
+          </template>
+        </div>
       </div>
-    </div>
-
-    <!-- Right Element -->
-    <div class="flex flex-row gap-2 items-center">
-      <template v-for="control in secondaryRightControls" :key="control.id">
-        <button
-          v-if="control.type === 'button'"
-          @click="emit('action', control.id)"
-          :class="getButtonClass(control.position, control.destructive)"
-        >
-          {{ control.label }}
-        </button>
-        <button
-          v-else-if="control.type === 'icon-button'"
-          @click="emit('action', control.id)"
-          :class="getButtonClass(control.position)"
-        >
-          <component :is="getIcon(control.icon)" />
-          <span v-if="control.label">{{ control.label }}</span>
-        </button>
-      </template>
     </div>
   </div>
 </template>
